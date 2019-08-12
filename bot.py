8e4a6d0282944cb39244411c60e3e5310af938f1
Qwantman <54003572+Qwantman@users.noreply.github.com>
2019-08-12 15:22:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,17 +3,18 @@ import vk_api
 import random
 import time
 def send_message(message=None, attachment=None):
-  vk_session.method('messages.send', {"user_id": event.user_id, "message": message, "attachment": attachment, "random_id": random.ra$
-
+  vk_session.method('messages.send', {"user_id": event.user_id, "message": message, "attachment": attachment, "random_id": random.randint(-2147483648,+2147483648)})
+#для ЛС
 def send_chat(message=None, attachment=None):
-  vk_session.method('messages.send', {"chat_id": event.chat_id, "message": message, "attachment": attachment, "random_id": random.ra$
-
+  vk_session.method('messages.send', {"chat_id": event.chat_id, "message": message, "attachment": attachment, "random_id": random.randint(-2147483648,+2147483648)})
+#для беседок
 token = "d18d76cc11b8c219d368cd861818c86821ec2b595d9bd9dbf1ff804dfbd2185c9826696e48accd3c0364c"
 vk_session = vk_api.VkApi(token = token)
 session_api = vk_session.get_api()
 longpoll = VkLongPoll(vk_session)
 start_time = time.monotonic()
 print('Бот запущен!')
+#если не выдало ошибок, тогда ща пойдут логи если кто-то напишет
 while True:
   for event in longpoll.listen():
     if(event.type == VkEventType.MESSAGE_NEW):
