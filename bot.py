f51bdd19ca78c2b721accede1d30f15684d7f254
Qwantman <54003572+Qwantman@users.noreply.github.com>
2019-08-12 12:21:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,14 @@
 from vk_api.longpoll import VkLongPoll, VkEventType
 import vk_api
 import random
-from datetime import datetime
 
-def send_message(message):
-  vk_session.method('messages.send', {"user_id": event.user_id, "message": message, "random_id": random.randint(-2147483648,+2147483648)})
+def send_message(message=None, attachment=None):
+  vk_session.method('messages.send', {"user_id": event.user_id, "message": message, "attachment": attachment, "random_id": random.randint(-2147483648,+2147483648)})
+  
+def send_chat(message=None, attachment=None):
+  vk_session.method('messages.send', {"chat_id": event.chat_id, "message": message, "attachment": attachment "random_id": random.randint(-2147483648,+2147483648)})
 
-def send_chat_msg(message):
-  vk_session.method('messages.send', {"chat_id": event.chat_id, "message": message, "random_id": random.randint(-2147483648,+2147483648)})
-
-token = "d18d76cc11b8c219d368cd861818c86821ec2b595d9bd9dbf1ff804dfbd2185c9826696e48accd3c0364c" 
+token = "YourToken" 
 vk_session = vk_api.VkApi(token = token)
 session_api = vk_session.get_api()
 longpoll = VkLongPoll(vk_session)
@@ -20,12 +19,9 @@ while True:
       print('Текст сообщения: '+ str(event.text))
       response = event.text.lower()
       if(event.from_user and not event.from_me):
-        if(response != ' '):
-          if(response != ' '):
-            break
-        else:
-          message = input('Введите сообщение в ответ: ')
-          send_message(message)
-      elif(event.from_chat):
+        if(response == 'бот' or 'bot'):
+          print('Пользователь с id:' +str(event.user_id) +"запросил команду bot")
+          send_message(message="Бот работает исправно.", attachment="")
+      elif(event.from_chat and not event.from_me):
         message = input('Введите сообщение в ответ: ')
-        send_message(message)
+        send_chat(message)
